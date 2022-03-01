bb2c91bcd6358412e1b3268d9c981a7468a85c09
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-03-01 13:28:18 +0530

    Embedding expressions

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends React.Component {
-  render() { 
+  state = {
+    count: 0
+  }
+
+  render() {
     return (
       <React.Fragment>
-        <h1>Hello World!!!</h1>
+        <span>count = {this.state.count}</span><br></br>
+        <span>count = {this.formatCount()}</span><br></br>
+        <span>2 + 2 = {2 + 2}</span><br></br>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state
+    const headingZero = <h1>Zero</h1>
+    return count === 0 ? headingZero : count
   }
 }
  
