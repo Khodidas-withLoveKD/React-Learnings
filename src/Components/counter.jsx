b930bca6ca5ddf0b6cbb3ded4e524fd6cae5a4c6
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-03-01 19:16:01 +0530

    rendering list

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,39 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Counter extends React.Component {
-  state = {
-    count: 0
-  };
-  
-  styles = {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'black'
-  };
+	state = {
+		count: 0,
+		tags: ['tag1', 'tag2', 'tag3']
+	}
 
-  render() {
-    return (
-      <React.Fragment>
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button style = {{ fontSize: 30, fontWeight: 'bolder' }} className="btn btn-warning btn-sm">
-          Increment
-        </button>
-      </React.Fragment>
-    );
-  }
+	styles = {
+		fontSize: 20,
+		fontWeight: 'bold',
+		color: 'black'
+	}
 
-  getBadgeClasses() {
-    let classes = 'badge m-2 bg-';
-    classes += (this.state.count === 0) ? 'warning' : 'primary';
-    return classes;
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<span style={this.styles} className={this.getBadgeClasses()}>
+					{this.formatCount()}
+				</span>
+				<button
+					style={{ fontSize: 30, fontWeight: 'bolder' }}
+					className="btn btn-warning btn-sm"
+				>
+					Increment
+				</button>
+				<ul>
+					{this.state.tags.map((tag) => (
+						<li key={tag}>{tag}</li>
+					))}
+				</ul>
+			</React.Fragment>
+		)
+	}
 
-  formatCount() {
-    const { count } = this.state
-    return count === 0 ? 'Zero' : count
-  }
+	getBadgeClasses() {
+		let classes = 'badge m-2 bg-'
+		classes += this.state.count === 0 ? 'warning' : 'primary'
+		return classes
+	}
+
+	formatCount() {
+		const { count } = this.state
+		return count === 0 ? 'Zero' : count
+	}
 }
- 
-export default Counter;
+
+export default Counter
