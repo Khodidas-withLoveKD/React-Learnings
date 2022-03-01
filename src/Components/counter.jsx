edd8afd37908921b31fcc07e6c38fab45192c314
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-03-01 17:29:11 +0530

    Add class dynamically

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -4,14 +4,17 @@ class Counter extends React.Component {
   state = {
     count: 0
   };
+  
   styles = {
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    color: 'black'
   };
+
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge bg-primary m-2">
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button style = {{ fontSize: 30, fontWeight: 'bolder' }} className="btn btn-warning btn-sm">
@@ -19,6 +22,12 @@ class Counter extends React.Component {
         </button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 bg-';
+    classes += (this.state.count === 0) ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCount() {
