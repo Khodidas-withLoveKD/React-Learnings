08da2a0cc58e3b8cc29cb1c15d56df60ca518692
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-03-08 14:07:48 +0530

    (What is) Single source of truth

--- src/Components/Counters.jsx
+++ src/Components/Counters.jsx
@@ -11,6 +11,14 @@ class Counters extends React.Component {
 			{ id: 4, value: 3 },
 		],
 	}
+	resetCounters = () => {
+		console.log('Resetting counter')
+		const counters = this.state.counters.map((counter) => {
+			counter.value = 0
+			return counter
+		})
+		this.setState({ counters })
+	}
 	deleteCounter = (counterId) => {
 		console.log('Counter deleted with ID = ', counterId)
 		const counters = this.state.counters.filter((c) => c.id !== counterId)
@@ -19,6 +27,12 @@ class Counters extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
+				<button
+					onClick={this.resetCounters}
+					className="btn btn-primary btn-sm m-2"
+				>
+					Reset
+				</button>
 				{this.state.counters.map((counter) => (
 					<Counter
 						key={counter.id}
