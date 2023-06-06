47553c15b314554173dbad1b706c02cb521e771c
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2023-06-06 13:04:34 +0530

    Completed Xlsx Parsing and rendering

--- src/Pages/WorkExperiments/XlsxCsvEditor/XlsxCsvEditor.jsx
+++ src/Pages/WorkExperiments/XlsxCsvEditor/XlsxCsvEditor.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
-import readXlsxFile from 'read-excel-file';
 
 import CsvParser from './CsvParser';
-import { extractDataFromCsvFile } from "./editorHelper";
+import XlsxParser from "./XlsxParser";
 
 const XlsxCsvEditor = () => {
   const [file, setFile] = useState(null);
-  const [fileData, setFileData] = useState([]);
   const [showCsvParser, setCsvParserVisibility] = useState(false);
+  const [parseFile, setParseFile] = useState(false);
 
   const detectFileType = () => {
-    debugger
+    // debugger
     const fileExtension = file.name.split('.').pop().toLowerCase();
     if (fileExtension === 'csv') {
       setCsvParserVisibility(true)
+      setParseFile(true)
     } else if (fileExtension === 'xlsx') {
       setCsvParserVisibility(false)
+      setParseFile(true)
     } else {
-      console.log('Error')
+      console.log('kd Error')
     }
   } 
 
@@ -26,44 +27,6 @@ const XlsxCsvEditor = () => {
     setFile(uploadedFile)
   }
 
-  const parseCSVFile = () => {
-    if (!file) {
-      alert('No File Selected')
-      return
-    }
-
-    // csv reading
-    const reader = new FileReader()
-    reader.onload = (event) => {
-      console.log('kd file: ', file)
-      console.log('kd event:', event)
-      console.log('kd event.target.result:', event.target.result)
-      setFileData(event.target.result)
-      const {columns, rows } = extractDataFromCsvFile(event.target.result)
-      console.log('kd rows:', rows)
-      console.log('kd columns:', columns)
-      // pass rows and columns to reactDataGrid
-    }
-    reader.readAsText(file)
-  }
-
-  const paserXlsxFile = () => {
-    if (!file) {
-      alert('No File Selected')
-      return
-    }
-
-    readXlsxFile(file).then((rows) => {
-      console.log('kd file:', file)
-      // Process the parsed rows here
-      console.log('rows = ', rows);
-    }).catch((error) => {
-      // Handle any errors that occur during parsing
-      console.error(error);
-    });
-  }
-
-
   return (
     <div>
       <h2>CSV / XLSX</h2>
@@ -71,7 +34,7 @@ const XlsxCsvEditor = () => {
       <br />
       <button onClick={detectFileType}>Parse File</button>
       <hr />
-      {showCsvParser ? <CsvParser file={file} /> : <></>}
+      {parseFile && (showCsvParser ? <CsvParser file={file} /> : <XlsxParser file={file}/>)}
     </div>  
     );
 }
---
+++ src/Pages/WorkExperiments/XlsxCsvEditor/XlsxParser.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react"
+import React from "react"
+import { textEditor } from "react-data-grid"
+import readXlsxFile from 'read-excel-file';
+
+import ReactDataGrid from "./ReactDataGrid"
+
+interface IXlsxParser {
+  file: File
+}
+
+interface IColumnForReactDataGrid {
+  key: string;
+  name: string;
+  
+}
+
+const XlsxParser = (props: IXlsxParser) => {
+  const { file } = props
+
+  const [columns, setColumns] = useState<Array<any>>([])
+  const [rows, setRows] = useState<Array<any>>([])
+
+  useEffect(() => {
+    extractDataFromXlsxFileAndGenerateRowsAndColumns()
+  }, [])
+
+  const extractDataFromXlsxFileAndGenerateRowsAndColumns = () => {
+    readXlsxFile(file).then((fileRows) => {
+      // Process the parsed rows here
+      const extractedColumns:any = fileRows[0]
+      const extractedRows:any = fileRows.slice(1)
+      // console.log('kd INSIDE extractedRows:', extractedRows)
+      generateRowAndColumsForReactDataGrid(extractedRows, extractedColumns);
+    }).catch((error) => {
+      // Handle any errors that occur during parsing
+      console.error("kd error: ", error);
+    })
+  }
+
+  const generateRowAndColumsForReactDataGrid = (rows: Array<any>, columns:any) => {
+
+    const generateColumns = () => {
+      const columnsForReactDataGrid = [{
+        key: 'rowNo', name: '', frozen: true, width: 'max-content'
+      }]
+      
+      columns.forEach((columnName:any) => {
+        const columnObject = {
+          key: columnName,
+          name: columnName,
+          width: 'max-content',
+          resizable: true,
+          editable: false,
+          editor: textEditor,
+          frozen: false
+        }
+        columnsForReactDataGrid.push(columnObject)
+      })
+      setColumns(columnsForReactDataGrid)
+    }
+
+    const generateRows = () => {
+      const rowsForReactDataGrid:any = []
+      rows.forEach((row: [], rowIndex: number) => {
+        // const cellValues = row.split(',')
+        const rowObject:any = {}
+        row.forEach((cellValue: string, index:number) => {
+          rowObject[columns[index]] = cellValue
+        })
+        rowObject['rowNo'] = rowIndex + 1
+        rowsForReactDataGrid.push(rowObject)
+      })
+      setRows(rowsForReactDataGrid)
+    }
+
+    generateColumns()
+    generateRows()
+  }
+
+  return (<ReactDataGrid columns={columns} rows={rows} setRows={setRows}/>)
+}
+ 
+export default XlsxParser;
