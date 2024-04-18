d845d296ca12f4d33f74a947e175c50b87eb7941
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2024-04-18 19:11:15 +0530

    Learnt basics of customHooks and created custom useForm Hook | Leaving it for now as ref is causing issue

--- src/Pages/ReactBestPractices/CustomHooks/KDReactHookForm.tsx
+++ src/Pages/ReactBestPractices/CustomHooks/KDReactHookForm.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import useKDFormHook from './useKDFormHook'
 
 enum FormId {
 	NAME = 'name',
@@ -28,7 +29,7 @@ const KDReactHookForm = () => {
 				right: 0,
 				top: 0,
 				backgroundColor: 'rgba(0, 0, 0, 0.4)',
-				position: 'absolute',
+				position: 'fixed',
 			},
 		}
 
@@ -59,30 +60,73 @@ const KDReactHookForm = () => {
 		)
 	}
 
+	// Hook Form related things
+	// ≠≠≠≠======≠≠≠=======≠≠≠
+
 	const { NAME, EMAIL, PHONE } = FormId
 
-	const handleChange = (e: any) => {
-		console.log('kd e:', e)
+	const { setValue, getValues, formValues, onChange } = useKDFormHook()
+	console.log('kd formValues:', formValues)
+	console.log('kd formValues.name:', formValues.NAME)
+	console.log('kd formValues[NAME]:', formValues[NAME])
+	const [name, setValuesetName] = useState<string>('')
+	const inputRef = useRef<HTMLInputElement | null>(null)
+	const [email, setEmail] = useState<string>('')
+	const emailRef = useRef<HTMLInputElement | null>(null)
+
+	const handleChange = (key: FormId, value: string | number) => {
+		// e.preventDefault()
+		// setValue(key, e.target.value)
+		setValue(key, value)
 	}
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault()
 		console.log('kd e:', e)
 	}
+	// console.log('kd getValues(NAME):', getValues(NAME))
 
 	const KDsForm = () => (
 		<form onSubmit={handleSubmit}>
 			<div>
 				<label htmlFor={NAME}>Enter Name</label>
-				<input type="text" id={NAME} onChange={handleChange} name={NAME} />
+				<input
+					type="text"
+					id={NAME}
+					// onChange={(e: any) => handleChange(NAME, e.target.value)}
+					onChange={onChange}
+					name={NAME}
+					value={formValues[NAME]}
+					// ref={(ref) => {
+					// 	console.log('kd ref:', ref)
+					// 	inputRef.current = ref
+					// 	inputRef.current?.focus()
+					// }}
+				/>
 			</div>
 			<div>
 				<label htmlFor={EMAIL}>Enter Email</label>
-				<input type={'email'} id={EMAIL} onChange={handleChange} />
+				<input
+					type={'email'}
+					id={EMAIL}
+					onChange={(e: any) => {
+						handleChange(EMAIL, e.target.value)
+						emailRef.current?.focus()
+					}}
+					value={email}
+					// ref={(ref) => {
+					// 	emailRef.current = ref
+					// 	emailRef.current?.focus()
+					// }}
+				/>
 			</div>
 			<div>
 				<label htmlFor={PHONE}>Enter Phone</label>
-				<input type="number" id={PHONE} onChange={handleChange} />
+				<input
+					type="number"
+					id={PHONE}
+					onChange={(e: any) => handleChange(e, PHONE)}
+				/>
 			</div>
 			<button type="submit">Submit</button>
 		</form>
--- src/Pages/ReactBestPractices/CustomHooks/useKDFormHook.tsx
+++ src/Pages/ReactBestPractices/CustomHooks/useKDFormHook.tsx
@@ -4,20 +4,36 @@ import { IObjProps } from '../../../shared/interfaces'
 const useKDFormHook = () => {
 	const [formValues, setFormValues] = useState<IObjProps>({})
 
+	const onChange = (event: any) => {
+		const { name, value } = event.target
+		setFormValues({
+			...formValues,
+			[name]: value,
+		})
+	}
+
 	const setValue = (key: string, value: string | number) => {
+		// console.log('kd value:', value)
+		// console.log('kd key:', [key])
 		setFormValues((existingValues: IObjProps) => {
+			// console.log('kd existingValues:', existingValues)
 			const modifiedValues = {
 				...existingValues,
-				key: value,
+				[key]: value,
 			}
+			// console.log('kd modifiedValues:', modifiedValues)
 
 			return modifiedValues
 		})
 	}
 
-	const getValues = (key: string) => formValues[key]
+	const getValues = (key: string) => {
+		// console.log('kd key:', key)
+		// console.log('kd formValues[key]:', formValues[key])
+		return formValues[key]
+	}
 
-	return { setValue, getValues }
+	return { setValue, getValues, formValues, onChange }
 }
 
 export default useKDFormHook
