6f55460e900bbf05a1611e5c4ccd1c822a0daaff
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2024-09-05 20:17:55 +0530

    Added debounce effect with leading debounce effect which ignores the succeeding actions

--- src/Pages/InterveiewPrep/Debounce/Debounce.tsx
+++ src/Pages/InterveiewPrep/Debounce/Debounce.tsx
@@ -19,9 +19,34 @@ const Debounce = () => {
 
 	const processChange = debounce((e: any) => onSearchInputChange(e), 2000)
 
+	const ignoreFirstAndDebounce = (func: Function, timeout: number) => {
+		let timer2: any
+		return (...args: any) => {
+			console.log('kd timer:', timer2)
+			if (!timer2) {
+				func(args)
+			}
+
+			// This will make the user wait for `timeout` time before letting him make the search query again
+			clearTimeout(timer2)
+			timer2 = setTimeout(() => {
+				timer2 = undefined
+			}, timeout)
+		}
+	}
+
+	const processIgnoreFirstDebounce = ignoreFirstAndDebounce(
+		(e: any) => onSearchInputChange(e),
+		2000
+	)
+
 	return (
 		<>
+			Debounce effect
 			<input onChange={processChange} />
+			<br />
+			Debounce effect with ignore first
+			<input onChange={processIgnoreFirstDebounce} />
 		</>
 	)
 }
